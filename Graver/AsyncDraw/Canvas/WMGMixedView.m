bbbdd134d63da48437642c5469b29ee531ef34d1
江伟 <jiangwei21@meituan.com>
2019-04-17 19:33:02 +0800

    修改WMGMixedView图片加载
    Signed-off-by: 江伟 <jiangwei21@meituan.com>

--- Graver/AsyncDraw/Canvas/WMGMixedView.m
+++ Graver/AsyncDraw/Canvas/WMGMixedView.m
@@ -213,17 +213,16 @@ NSString * const WMGMixedViewTextVerticalAlignmentKey = @"waimai-graver-mixedvie
             if (att.type == WMGAttachmentTypeStaticImage){
                 WMGImage *gImage = (WMGImage *)att.contents;
                 if ([gImage isKindOfClass:[WMGImage class]]) {
-                    
-//                    [gImage wmg_loadImageWithUrl:gImage.downloadUrl options:0 progress:NULL completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
-//
-//                        [_lock lock];
-//                        if ([_arrayAttachments containsObject:att]) {
-//                            [_arrayAttachments removeObject:att];
-//                            i--;
-//                            [self setNeedsDisplay];
-//                        }
-//                        [_lock unlock];
-//                    }];
+                    [gImage wmg_loadImageWithUrl:gImage.downloadUrl options:0 progress:NULL completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+
+                        [self.lock lock];
+                        if ([self.arrayAttachments containsObject:att]) {
+                            [self.arrayAttachments removeObject:att];
+                            i--;
+                            [self setNeedsDisplay];
+                        }
+                        [self.lock unlock];
+                    }];
                 }
             }
         }
